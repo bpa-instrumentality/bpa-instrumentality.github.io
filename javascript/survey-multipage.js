4a46339eddce39ac2c20c9561046d5fb3685c33c
Ninjagor <rohitismaster@gmail.com>
2022-11-28 18:18:57 -0500

    Added a logo as well as started the helper survey page

---
+++ javascript/survey-multipage.js
@@ -0,0 +1,9 @@
+let currentPage = 1;
+
+let page1 = document.getElementById('page1');
+let page2 = document.getElementById('page2');
+let page3 = document.getElementById('page3');
+
+page1.style.display = "flex";
+page2.style.display = "none";
+page3.style.display = "none";
