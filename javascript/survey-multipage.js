0576e574c0fb17dc4d6bcfe727a29f69b6efeeca
Ninjagor <rohitismaster@gmail.com>
2022-11-29 17:52:45 -0500

    Worked on the questions page some more

--- javascript/survey-multipage.js
+++ javascript/survey-multipage.js
@@ -3,7 +3,31 @@ let currentPage = 1;
 let page1 = document.getElementById('page1');
 let page2 = document.getElementById('page2');
 let page3 = document.getElementById('page3');
+let btns = document.querySelectorAll('.btn');
+
 
 page1.style.display = "flex";
 page2.style.display = "none";
 page3.style.display = "none";
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        if (btn.textContent === "Go Back") {
+            if (!(currentPage < 2)) {
+                document.getElementById('page'+ currentPage).style.display = "none";
+                currentPage -= 1;
+                document.getElementById('page'+ currentPage).style.display = "flex";
+            }
+            else {
+                btn.disabled = "true";
+            }
+        } else {
+            if (!(currentPage > 2)) {
+                document.getElementById('page'+ currentPage).style.display = "none";
+                currentPage += 1;
+                document.getElementById('page'+ currentPage).style.display = "flex";
+            } else {
+                btn.disabled = "true";
+            }
+        }
+    })
+})
