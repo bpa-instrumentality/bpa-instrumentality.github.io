1e21cb6c12c8a932914baf72b3773aad7ff5534c
Ninjagor <rohitismaster@gmail.com>
2022-12-25 16:09:33 -0500

    Incorporated FUSE.js as the sorting algorithm

--- javascript/instruments-loader.js
+++ javascript/instruments-loader.js
@@ -24,18 +24,68 @@ fetch("./instruments.json")
     .then((response) => response.json())
     .then(json => {
         let instruments = json.instruments;
-        let originalFilter = "118437";
-        let filter = originalFilter;
-        let newArr = []
-        instruments.sort((a, b) => a.filterid - b.filterid)
-        console.log(instruments)
-        instruments.sort((a, b) => {
-            if (b.filterid.toString().includes(filter)) {
-                return 1
+        let filter = 13;
+        // const sorted = [...instruments].sort((a, b) => {
+        //     const aValue = getMatchValue(filter, a.filterid)
+        //     const bValue = getMatchValue(filter, b.filterid)
+        //     return aValue < bValue ? 1 : aValue > bValue ? -1 : 0
+        //   })
+          
+        //   console.log(sorted)
+          
+        //   function getMatchValue(original, filterid) {
+        //     const a = original.toString()
+        //     const b = filterid.toString()
+          
+        //     let sum = 0
+          
+        //     for (let i = 0; i < a.length; i += 1) {
+        //       if (a[i] === b[i]) {
+        //         sum += a.length - i
+        //       }
+        //     }
+          
+        //     return sum
+        //   }
+
+          const options = {
+            includeScore: true,
+            keys: [{
+                name: "nameid",
+                weight: 0.7
+            },
+            {
+                name: "filterid",
+                weight: 0.3
+            },
+            {
+                name: "priceid",
+                weight: 0.1
             }
-            else {
-                return -1
-            }
-        })
-        console.log(instruments)
+        ]
+          }
+
+          const fuse = new Fuse(instruments, options)
+          const sorted = fuse.search("118236")
+
+          console.log('fuse: ', sorted)
+
+        for (i in sorted) {
+            let curr = sorted[i].item;
+            let irsCard = document.createElement('instrument-result-card');
+            irsCard.classList.add('irs')
+            irsCard.setAttribute('title', curr.title);
+            irsCard.setAttribute('picture', curr.picture);
+            let span1 = document.createElement('span')
+            span1.setAttribute('slot', "desc");
+            span1.textContent = curr.desc;
+            irsCard.appendChild(span1)
+            let span2 = document.createElement("span");
+            span2.setAttribute('slot', "price");
+            span2.textContent = curr.price;
+            irsCard.appendChild(span2)
+            document.getElementById('results').appendChild(irsCard);
+        }
+        document.getElementById('searching').style.display = "none"
+        
     })
